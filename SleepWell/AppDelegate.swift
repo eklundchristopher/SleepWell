047d4957bc9f50a03e4733bc17bc1f1e47b5ffa4
Christopher Eklund <eklundchristopher@gmail.com>
2017-07-02 21:28:10 +0200

    Initial Commit

---
+++ SleepWell/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SleepWell
+//
+//  Created by Christopher Eklund on 2017-07-02.
+//  Copyright © 2017 Christopher Eklund. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SleepWell/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SleepWell
+//
+//  Created by Christopher Eklund on 2017-07-02.
+//  Copyright © 2017 Christopher Eklund. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
