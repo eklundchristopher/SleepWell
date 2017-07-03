c8ab1f5fec70abd837bd8db7d6376196af789598
Christopher Eklund <eklundchristopher@gmail.com>
2017-07-03 17:30:51 +0200

    Fixed preferences

--- SleepWell/MenubarController.swift
+++ SleepWell/MenubarController.swift
@@ -22,6 +22,8 @@ class MenubarController: NSObject {
     var interval = 0
     dynamic var enabled: NSNumber? = false
     dynamic var disabled: NSNumber? = true
+    dynamic var isSavingInterval: NSNumber? = false
+    dynamic var prefersSleeping: NSNumber? = false
     var timeRemaining = 0
     var timer = Timer()
     let menubarIcon = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
@@ -46,6 +48,16 @@ class MenubarController: NSObject {
         setTime(minutes: sender.integerValue)
     }
     
+    @IBAction func toggleSaveInterval(_ sender: NSButton) {
+        isSavingInterval = sender.state as NSNumber
+        writePlist(namePlist: "Preferences", key: "Save Interval", data: isSavingInterval!)
+    }
+    
+    @IBAction func togglePrefersSleeping(_ sender: NSButton) {
+        prefersSleeping = sender.state as NSNumber
+        writePlist(namePlist: "Preferences", key: "Sleep", data: prefersSleeping!)
+    }
+    
     override func awakeFromNib() {
         let icon = NSImage(named: "AppIcon")
         icon?.isTemplate = true
@@ -54,7 +66,16 @@ class MenubarController: NSObject {
         menubarIcon.image = icon
         menubarIcon.menu = menubar
         
-        setTime(minutes: 60)
+        isSavingInterval = readPlist(namePlist: "Preferences", key: "Save Interval") as? NSNumber
+        prefersSleeping = readPlist(namePlist: "Preferences", key: "Sleep") as? NSNumber
+        
+        var minutes = 60
+        let savedMinutes = readPlist(namePlist: "Preferences", key: "Interval") as! Int
+        if savedMinutes > 0 {
+            minutes = savedMinutes
+        }
+        
+        setTime(minutes: minutes)
         setControlButtonStates()
     }
     
@@ -88,6 +109,10 @@ class MenubarController: NSObject {
         
         intervalStepper.stringValue = String(getTime())
         intervalLabel.stringValue = String(getTime()) + " minutes"
+        
+        if isSavingInterval?.boolValue == true {
+            writePlist(namePlist: "Preferences", key: "Interval", data: (interval / 60) as NSNumber)
+        }
     }
     
     func startTimer() {
@@ -108,7 +133,11 @@ class MenubarController: NSObject {
     }
     
     func shutdownComputer() {
-        NSAppleScript(source: "tell application \"Finder\"\nshut down\nend tell")!.executeAndReturnError(nil)
+        if prefersSleeping == true {
+            NSAppleScript(source: "tell application \"Finder\"\nsleep\nend tell")!.executeAndReturnError(nil)
+        } else {
+            NSAppleScript(source: "tell application \"Finder\"\nshut down\nend tell")!.executeAndReturnError(nil)
+        }
     }
     
     func timerRunning() {
@@ -126,4 +155,60 @@ class MenubarController: NSObject {
             stopTimer()
             shutdownComputer()
         }
-    }}
+    }
+    
+    func readPlist(namePlist: String, key: String) -> AnyObject{
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) as NSArray
+        let documentsDirectory = paths.object(at: 0) as! NSString
+        let path = documentsDirectory.appendingPathComponent(namePlist+".plist")
+        
+        var output:Any = false
+        
+        if let dict = NSMutableDictionary(contentsOfFile: path){
+            output = dict.object(forKey: key)!
+        }else{
+            if let privPath = Bundle.main.path(forResource: namePlist, ofType: "plist"){
+                if let dict = NSMutableDictionary(contentsOfFile: privPath){
+                    output = dict.object(forKey: key)!
+                }else{
+                    output = false
+                    print("error_read")
+                }
+            }else{
+                output = false
+                print("error_read")
+            }
+        }
+        return output as AnyObject
+    }
+    
+    func writePlist(namePlist: String, key: String, data: AnyObject){
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) as NSArray
+        let documentsDirectory = paths.object(at: 0) as! NSString
+        let path = documentsDirectory.appendingPathComponent(namePlist+".plist")
+        
+        if let dict = NSMutableDictionary(contentsOfFile: path){
+            dict.setObject(data, forKey: key as NSCopying)
+            if dict.write(toFile: path, atomically: true){
+                print("plist_write")
+            }else{
+                print("plist_write_error")
+            }
+        }else{
+            if let privPath = Bundle.main.path(forResource: namePlist, ofType: "plist"){
+                if let dict = NSMutableDictionary(contentsOfFile: privPath){
+                    dict.setObject(data, forKey: key as NSCopying)
+                    if dict.write(toFile: path, atomically: true){
+                        print("plist_write")
+                    }else{
+                        print("plist_write_error")
+                    }
+                }else{
+                    print("plist_write")
+                }
+            }else{
+                print("error_find_plist")
+            }
+        }
+    }
+}
